dc9ed5d0b6268bbf7a453130f9fa9f211ae14666
xcadis <joeypunzel@gmail.com>
2021-05-17 12:20:59 -0700

    fix typo on model.js

--- backend/models/models.js
+++ backend/models/models.js
@@ -74,7 +74,7 @@ export const insertCategory = (data, result) => {
 }
 
 // Get All Users
-export const getCategories = (result) => {
+export const getUsers = (result) => {
     db.query("select username,isAdmin from users", (err, results) => {             
         if(err) {
             console.log(err);
