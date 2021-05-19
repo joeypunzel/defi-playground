49ea4d1dee8dc82b6e2cf296d3a7f39a08efb45f
xcadis <joeypunzel@gmail.com>
2021-05-19 11:37:58 -0700

    one more fix on backend

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,56 +1,36 @@
-
-//module.exports = app => {
-//const queries = require("../controllers/controllers.js");
-//}
-
 const db = require("../config/database.js");
 const express = require('express');
 const router = express.Router();
-const funcs = require("../controllers/controllers.js")
+const controllers = require("../controllers/controllers.js")
 
-
- 
 // Get All Project
-router.get('/projectList',funcs.showProjects);
-// get user lists
-
-
-//router.get('/projectList', function(req, res) {
-//    let sql = `select p.projectID,p.projectName,p.marketCap, b.blockchainName,c.categoryName, p.description,p.inceptionDate from projects p left join blockchains b on p.blockchainID = b.blockchainID left join categories c on p.categoryID = c.categoryID;`;
-//   db.query(sql, function(err, data, fields) {
-//        if (err) throw err;
-//        res.json({
-//            data
-//        })
-//      })  
-//  });
-
+router.get('/projectList',controllers.showProjects);
 
 // Create New Project
-//app.post('/projectList', queries.createProject);
+router.post('/projectList', controllers.createProject);
 
 // Get All Blockchains
-//app.get('/blockchainList', queries.showBlockchains);
+router.get('/blockchainList', controllers.showBlockchains);
 
 // Create New Blockchain
-//app.post('/blockchainList', queries.createBlockchain);
+router.post('/blockchainList', controllers.createBlockchain);
 
 // Get All Categories
-//app.get('/categoryList', queries.showCategories);
+router.get('/categoryList', controllers.showCategories);
 
 // Create New Blockchain
-//app.post('/categoryList', queries.createCategory);
+router.post('/categoryList', controllers.createCategory);
 
 // Get All Users
-//app.get('/userList', queries.showUsers);
+router.get('/userList', controllers.showUsers);
 
 // Create New User
-//app.post('/userList', queries.createUser);
+router.post('/userList', controllers.createUser);
 
 // Get All Favorites
-//app.get('/favoriteList', queries.showFavorites);
+router.get('/favoriteList', controllers.showFavorites);
 
 // Create New Favorite
-//app.post('/favoriteList', queries.createFavorite);
+router.post('/favoriteList', controllers.createFavorite);
 
 module.exports = router;
